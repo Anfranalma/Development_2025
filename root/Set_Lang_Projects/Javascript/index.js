b84d0571320c83373d8477aa04538a6f135deea2
Angel Grove <angel.grove@gmail.com>
2025-11-14 16:12:41 -0500

    javascript

---
+++ root/Set_Lang_Projects/Javascript/index.js
@@ -0,0 +1,9 @@
+const PORT = 8000
+
+const axios = require("axios")
+const cheerios = require("cheerio")
+const express = require("express")
+
+const app = express()
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`) )
