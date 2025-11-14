1759dcade3a6b0e28505845020259e3682c9df20
Angel Grove <angel.grove@gmail.com>
2025-11-14 16:36:59 -0500

    csharp projects

---
+++ root/Set_Lang_Projects/CSharp/MediaPlayer/WMNewBorn/WMNewBorn/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WMNewBorn
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool Playstate = false;
+        bool Stopstate = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Me.Play();
+            Playstate = true;
+            Stopstate = false;
+        }
+
+        private void StopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Me.Stop();
+            Playstate = false;
+            Stopstate = true;
+        }
+
+        private void PauseBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+            if (Playstate == false)
+            {
+                Me.Play();
+                Playstate = true;
+            }
+            else
+            {
+                Me.Pause();
+                Playstate = false;
+            }
+        }
+
+        private void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Me.Volume = VolumeSlider.Value;
+        }
+    }
+}
